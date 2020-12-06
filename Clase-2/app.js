b0c68031946eb1c22fc05c998bd5a310f1847fe1
PedroRivas96G <pedroguardado96@gmail.com>
2020-12-06 08:24:41 -0600

    trabjando con html y get

--- Clase-2/app.js
+++ Clase-2/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer ((req, res) =>{
     res.writeHead(200, {"Content-Type": "application/json"})
-    res.write('Hola Mundo')
+    res.write('Esta es una prueba de el servidor')
     res.end();
 })
 
